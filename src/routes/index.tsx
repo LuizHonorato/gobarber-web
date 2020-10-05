dee4f4ef775d09136d2687d36f8ebe788df620a3
Luiz Honorato <lhehonorato@gmail.com>
2020-10-04 23:13:41 -0300

    finalizing GoBarber Web.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,7 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import Profile from '../pages/Profile';
 import Dashboard from '../pages/Dashboard';
 import ForgotPassword from '../pages/ForgotPassword';
 import ResetPassword from '../pages/ResetPassword';
@@ -16,6 +17,7 @@ const Routes: React.FC = () => (
     <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/reset-password" component={ResetPassword} />
 
+    <Route path="/profile" isPrivate component={Profile} />
     <Route path="/dashboard" isPrivate component={Dashboard} />
   </Switch>
 );
